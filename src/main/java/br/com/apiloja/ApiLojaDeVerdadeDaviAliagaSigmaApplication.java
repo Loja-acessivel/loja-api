d3cf4ed5bbfe8bcdd874af460b5a0e3c2725967a
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-06-09 16:13:22 -0300

    - feat/Projeto Spring

---
+++ src/main/java/br/com/apiloja/ApiLojaDeVerdadeDaviAliagaSigmaApplication.java
@@ -0,0 +1,13 @@
+package br.com.apiloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaDeVerdadeDaviAliagaSigmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLojaDeVerdadeDaviAliagaSigmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/apiloja/ApiLojaDeVerdadeDaviAliagaSigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiloja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaDeVerdadeDaviAliagaSigmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
