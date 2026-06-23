2b54e017cf2f67855edc70b11f169788c0144d96
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-06-22 21:36:43 -0300

    buscarTodos/Deletar/atualizar produto service

--- src/main/java/br/com/apiloja/Service/ProdutoService.java
+++ src/main/java/br/com/apiloja/Service/ProdutoService.java
@@ -5,8 +5,11 @@ import br.com.apiloja.Dto.Produto.ProdutoResponseDTO;
 import br.com.apiloja.Mapper.ProdutoMapper;
 import br.com.apiloja.Model.Produto;
 import br.com.apiloja.Repository.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -16,6 +19,30 @@ public class ProdutoService {
 
     public ProdutoResponseDTO inserir(ProdutoRequestDTO prod){
         Produto produto = mapper.toEntity(prod);
+        repo.save(produto);
+        return mapper.toResponse(produto);
+    }
+
+
+    public List<ProdutoResponseDTO> buscarTodos(){
+        List<Produto> produtos = repo.findAll();
+        return mapper.toResponseList(produtos);
+    }
+
+    public void deletar(Long id){
+        Produto produto = repo.findById(id).orElseThrow(() -> new EntityNotFoundException(""));
+        repo.delete(produto);
+    }
+
+    public ProdutoResponseDTO atualizar(Long id, ProdutoRequestDTO dto){
+        Produto produto = repo.findById(id).orElseThrow(() -> new EntityNotFoundException(""));
+
+        produto.setNome(dto.getNome());
+        produto.setCategoria(dto.getCategoria());
+        produto.setDescricao(dto.getDescricao());
+        produto.setPreco(dto.getPreco());
+        produto.setEstoque(dto.getEstoque());
+
         repo.save(produto);
         return mapper.toResponse(produto);
     }
