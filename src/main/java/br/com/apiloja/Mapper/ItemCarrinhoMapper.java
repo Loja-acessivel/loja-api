4d994a3744d3e1f3091d9549c55a5b7f848cf523
Lucas Almeida Da Costa <lucascosta-ieg@instituto.ad>
2026-07-20 11:50:09 -0300

    ItemCarrinhoMapper

--- src/main/java/br/com/apiloja/Mapper/ItemCarrinhoMapper.java
+++ src/main/java/br/com/apiloja/Mapper/ItemCarrinhoMapper.java
@@ -1,10 +1,19 @@
 package br.com.apiloja.Mapper;
 
-import br.com.apiloja.Dto.Carrinho.CarrinhoRequestDTO;
-import br.com.apiloja.Dto.Carrinho.CarrinhoResponseDTO;
 import br.com.apiloja.Dto.ItemCarrinho.ItemCarrinhoRequestDTO;
 import br.com.apiloja.Dto.ItemCarrinho.ItemCarrinhoResponseDTO;
-import br.com.apiloja.Model.Carrinho;
+import br.com.apiloja.Model.ItemCarrinho;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-public interface ItemCarrinhoMapper extends BaseMapper<ItemCarrinhoRequestDTO, ItemCarrinhoResponseDTO, Carrinho>{
+@Mapper(componentModel = "spring")
+public interface ItemCarrinhoMapper extends BaseMapper<ItemCarrinhoRequestDTO, ItemCarrinhoResponseDTO, ItemCarrinho>{
+    @Override
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "precoUnitario", ignore = true) // Será preenchido no Service com o preço do Produto
+    @Mapping(target = "subtotal", ignore = true)      // Calculado automaticamente pelo banco de dados
+    ItemCarrinho toEntity(ItemCarrinhoRequestDTO dto);
+
+    @Override
+    ItemCarrinhoResponseDTO toResponse(ItemCarrinho entity);
 }
